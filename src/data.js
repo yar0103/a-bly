5dc1053e51a2598e070fdf1e3d62a0df0bf14703
arin <yurins0103@naver.com>
2024-11-29 17:34:54 +0900

    이미지 경로 수정

--- src/data.js
+++ src/data.js
@@ -1,41 +1,41 @@
 const products = [
   {
-    image: '/commend-0.jpg',
+    image: process.env.BASE_URL + '/commend-0.jpg',
     discount: '35%',
     price: 29800,
     market: '모디무드',
     content: '핏보장 남성 슬랙스'
   },
   {
-    image: '/commend-1.jpg',
+    image: process.env.BASE_URL + '/commend-1.jpg',
     discount: '47%',
     price: 59900,
     market: '오카시',
     content: '따뜻한 여자 패딩자켓'
   },
   {
-    image: '/commend-2.jpg',
+    image: process.env.BASE_URL + '/commend-2.jpg',
     discount: '12%',
     price: 12000,
     market: '헤드델리',
     content: '얼굴 작아보이는 모자'
   },
   {
-    image: '/commend-3.jpg',
+    image: process.env.BASE_URL + '/commend-3.jpg',
     discount: '29%',
     price: 87900,
     market: '블랙업',
     content: '남녀노소 워너비 신발'
   },
   {
-    image: '/commend-4.jpg',
+    image: process.env.BASE_URL + '/commend-4.jpg',
     discount: '44%',
     price: 35200,
     market: '노이즈',
     content: '레더 미니어처 키링'
   },
   {
-    image: '/commend-5.jpg',
+    image: process.env.BASE_URL + '/commend-5.jpg',
     discount: '5%',
     price: 1963000,
     market: 'BURBERRY',
@@ -45,55 +45,58 @@ const products = [
 
 const shoppingCategory = [
   {
-    image: '/shopping-0.png',
+    image: process.env.BASE_URL + '/shopping-0.png',
     title: '의류'
   },
   {
-    image: '/shopping-1.png',
+    image: process.env.BASE_URL + '/shopping-1.png',
     title: '주얼리'
   },
   {
-    image: '/shopping-2.png',
+    image: process.env.BASE_URL + '/shopping-2.png',
     title: '디지털'
   },
   {
-    image: '/shopping-3.png',
+    image: process.env.BASE_URL + '/shopping-3.png',
     title: '신발'
   },
   {
-    image: '/shopping-4.png',
+    image: process.env.BASE_URL + '/shopping-4.png',
     title: '모자'
   },
   {
-    image: '/shopping-5.png',
+    image: process.env.BASE_URL + '/shopping-5.png',
     title: '가방'
   },
   {
-    image: '/shopping-6.png',
+    image: process.env.BASE_URL + '/shopping-6.png',
     title: '화장품'
   },
   {
-    image: '/shopping-7.png',
+    image: process.env.BASE_URL + '/shopping-7.png',
     title: '라이프'
   },
 ]
 
 const visual = [
   {
-    image: '/visual-1.jpg'
+    image: process.env.BASE_URL + '/visual-0.jpg'
   },
   {
-    image: '/visual-2.jpg'
+    image: process.env.BASE_URL + '/visual-1.jpg'
   },
   {
-    image: '/visual-3.jpg'
+    image: process.env.BASE_URL + '/visual-2.jpg'
   },
   {
-    image: '/visual-4.jpg'
+    image: process.env.BASE_URL + '/visual-3.jpg'
   },
   {
-    image: '/visual-5.jpg'
+    image: process.env.BASE_URL + '/visual-4.jpg'
+  },
+  {
+    image: process.env.BASE_URL + '/visual-5.jpg'
   },
 ]
 
-export {products, shoppingCategory, visual}
+export { products, shoppingCategory, visual }
